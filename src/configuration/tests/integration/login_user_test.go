56f4195304944409bd92f7442e83d691a87435c7
Gabriel Freitas <gabriielfs96@gmail.com>
2024-09-26 15:17:04 -0300

    test: add integration test

--- src/configuration/tests/integration/login_user_test.go
+++ src/configuration/tests/integration/login_user_test.go
@@ -50,6 +50,50 @@ func TestLoginUser(t *testing.T) {
 		UserController.CreateUser(ctxCreateUser)
 
 		userLoginRequest := request.UserLogin{
+			Email:    "test@notpassing.com",
+			Password: "ASDASD!@#3sss",
+		}
+
+		bLoginUser, _ := json.Marshal(userLoginRequest)
+		stringReaderLoginUser := io.NopCloser(strings.NewReader(string(bLoginUser)))
+
+		MakeRequest(ctxLoginUser, []gin.Param{}, url.Values{}, "POST", stringReaderLoginUser)
+		UserController.LoginUser(ctxLoginUser)
+
+		assert.EqualValues(t, http.StatusForbidden, recorderLoginUser.Result().StatusCode)
+		assert.NotEmpty(t, recorderLoginUser.Result().Header.Get("Authorization"))
+	})
+
+	t.Run("login_user_with_sucess", func(t *testing.T) {
+		recorderCreateUser := httptest.NewRecorder()
+		ctxCreateUser := GetTestGinContext(recorderCreateUser)
+
+		recorderLoginUser := httptest.NewRecorder()
+		ctxLoginUser := GetTestGinContext(recorderLoginUser)
+
+		email := fmt.Sprintf("%d@test.com", rand.Int())
+		password := "test@#123"
+
+		_, err := Database.Collection("test_users").InsertOne(context.Background(), bson.M{"name": t.Name(), "email": email})
+		if err != nil {
+			t.Fatal(err)
+			return
+		}
+
+		userRequest := request.UserRequest{
+			Email:    email,
+			Password: password,
+			Name:     "Test User",
+			Age:      32,
+		}
+
+		bCreateUser, _ := json.Marshal(userRequest)
+		stringReaderCreateUser := io.NopCloser(strings.NewReader(string(bCreateUser)))
+
+		MakeRequest(ctxCreateUser, []gin.Param{}, url.Values{}, "POST", stringReaderCreateUser)
+		UserController.CreateUser(ctxCreateUser)
+
+		userLoginRequest := request.UserLogin{
 			Email:    email,
 			Password: password,
 		}
